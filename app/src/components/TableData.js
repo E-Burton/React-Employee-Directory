ea85c4972d69a6bf2560ff07da9e6e49db92dee6
E-Burton <edwina.antoinette@gmail.com>
2021-03-06 23:09:56 -0700

    Adjusted Location column minWidth

--- app/src/components/TableData.js
+++ app/src/components/TableData.js
@@ -16,7 +16,8 @@ const columns = [
     {
         title: "Location",
         field: "location.country",
-        lookup: {"United States": "United States", "Canada": "Canada", "United Kingdom": "United Kingdom"}
+        lookup: {"United States": "United States", "Canada": "Canada", "United Kingdom": "United Kingdom"},
+        cellStyle: {minWidth: 200}
     },
     {
         title: "Phone",
