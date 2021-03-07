f5383de82afb785fa820cd8eaaf6178d24175f40
E-Burton <edwina.antoinette@gmail.com>
2021-03-06 23:26:08 -0700

    Updates

---
+++ precache-manifest.d8e62f639a57e39b5ada868769b75e33.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb378f18b087fefb6c02a54a70c07c41",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "8ce2da93ed617478fb7c",
+    "url": "/React-Employee-Directory/static/css/main.1a11bccc.chunk.css"
+  },
+  {
+    "revision": "3e3f16c67374c817f836",
+    "url": "/React-Employee-Directory/static/js/2.bedb0ae4.chunk.js"
+  },
+  {
+    "revision": "62533790c645b1ee0536",
+    "url": "/React-Employee-Directory/static/js/3.44eb41f0.chunk.js"
+  },
+  {
+    "revision": "e2234e5eae96132884c6",
+    "url": "/React-Employee-Directory/static/js/4.a07b7293.chunk.js"
+  },
+  {
+    "revision": "e7e6cf985c9ab3db37c1",
+    "url": "/React-Employee-Directory/static/js/5.ee65c3ce.chunk.js"
+  },
+  {
+    "revision": "110ca0b4c154a072a02c",
+    "url": "/React-Employee-Directory/static/js/6.a6791456.chunk.js"
+  },
+  {
+    "revision": "8ce2da93ed617478fb7c",
+    "url": "/React-Employee-Directory/static/js/main.9ff147a6.chunk.js"
+  },
+  {
+    "revision": "bde149365e2f03b76a8f",
+    "url": "/React-Employee-Directory/static/js/runtime~main.dab9e14e.js"
+  }
+]);
